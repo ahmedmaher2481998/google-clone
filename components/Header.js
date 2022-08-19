bd8ff58df16d99e8008bfa6e481ec12507569797
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-19 21:12:41 +0200

    add : fixing the link

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,27 @@
 import React from "react";
 import User from "./User";
-
+import Link from "next/link";
+import { useRouter } from "next/router";
 const Header = () => {
+	const router = useRouter;
 	return (
 		<>
 			<div className='flex  justify-between p-5'>
 				<div className='flex space-x-4 justify-center items-center'>
-					<p className='link_header'>About</p>
-					<p className='link_header'>Store</p>
+					<Link href='https://about.google/'>
+						<a className='link_header'>About</a>
+					</Link>
+					<Link href='https://store.google.com'>
+						<a className='link_header'>Store</a>
+					</Link>
 				</div>
 				<div className='flex space-x-4 justify-center items-center'>
-					<p className='link_header'>Gmail</p>
-					<p className='link_header'>Images</p>
+					<Link href={"https://gmail.com"}>
+						<a className='link_header'>Gmail</a>
+					</Link>
+					<Link href={`/search?q=${router.query?.q || "memes"}&category=image`}>
+						<a className='link_header'>Images</a>
+					</Link>
 					<User />
 				</div>
 			</div>
